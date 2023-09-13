13342880b2769f0c77d9f7f2ff92bda093b80fc9
dancer1325 <alfredotic0809@gmail.com>
2023-09-13 21:48:52 +0200

    doc(README.md): adjust README.adoc and add commentaries

--- complete/src/main/java/com/example/securingweb/MvcConfig.java
+++ complete/src/main/java/com/example/securingweb/MvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+	// Reference each viewController -> each view
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
